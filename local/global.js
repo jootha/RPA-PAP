b1d27ae3055cc8038641e6ccfe4bc238fe4e8740
lecomtad <adrien.lecomte.ext@ag2rlamondiale.fr>
2021-03-24 14:19:05 +0100

    init + 1e captures

---
+++ local/global.js
@@ -0,0 +1,31 @@
+﻿
+// *** Choose language (en|fr|de) ***
+GLOBAL.labels.setLanguage(e.language.English);
+
+// Global Systray object
+var systray = ctx.systray();
+
+
+/** main process start handler */
+GLOBAL.events.START.on(function (ev) {
+
+	systray.createSystrayMenu(ctx.options.projectName, 'ICON1');
+	systray.addMenu('', 'evMamba', GLOBAL.labels.scenarios.scMamba, '', function (ev) {
+		var rootData = ctx.dataManagers.rootData.create();
+		GLOBAL.scenarios.scMamba.start(rootData);
+	});
+});
+
+/** main process stop handler */
+GLOBAL.events.QUIT.on(function(ev) {
+	// add code here
+});
+
+/** Auto-update menu handler */
+GLOBAL.events.UPDATECTX.on(function(ev) {
+	ctx.shutdownAgent(true, true, (ctx.options.restartConfirmation ? GLOBAL.labels.updatePopup.label : null), GLOBAL.labels.updatePopup.title);
+});
+function traceErrorVariable(sc, stepName, gravity, codeError, error, todo, ex){
+	ag2r.audit.log("[ERROR] ERR" + codeError.toString() + " : " + error + " : " + ex, e.logIconType.Error);
+	ag2r.audit.failStep(sc.name, stepName, "ERR" + codeError.toString() + " : " + error + " :" + ex);
+}
